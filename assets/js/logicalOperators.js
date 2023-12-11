33dd8cf3e80b015f4fdcf8f5ecdd417b17a3c0f8
Yasir Mohamed <Yasirwiifto@gmail.com>
2023-12-11 12:35:37 +0000

    added logical operators

---
+++ assets/js/logicalOperators.js
@@ -0,0 +1,41 @@
+// Logical operators
+
+// Logical AND &&
+// Returns TRUE if both operands are TRUE 
+
+console.log(true && true); // output True
+
+console.log(false && true); // output False 
+
+// Real world example logical AND &&
+
+let highIncome = true;
+
+let goodCreditScore = true;
+
+let eligibleForLoan = highIncome && goodCreditScore;
+
+console.log('Eligible', eligibleForLoan);
+
+
+
+// Logical NOT (!)
+
+let applicationRefused = !eligibleForLoan;
+
+console.log('Not eligible', eligibleForLoan);
+
+
+// Logical OR ||
+// Returns TRUE if one operands is TRUE 
+
+// Real world example logical OR ||
+
+let  Score = true;
+
+let HighCreditScore = false;
+
+let Pass = highIncome || goodCreditScore;
+
+console.log(Pass);
+
