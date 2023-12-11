389b1471c10bd1673ea8368d3b3863f6b80c0e9f
Yasir Mohamed <Yasirwiifto@gmail.com>
2023-12-11 10:42:26 +0000

    added javascript Operators

---
+++ assets/js/javascriptOperators.js
@@ -0,0 +1,18 @@
+// Arithmetic Operators
+
+let x = 10;
+
+let y = 3;
+
+// console.log (x + y);
+
+// console.log (x - y);
+
+// console.log (x * y);
+
+// console.log (x / y);
+
+// console.log (x % y);
+
+// console.log (x ** y);
+
