922477d183d66df69af239728eaf67a0bc104c53
Yasir Mohamed <Yasirwiifto@gmail.com>
2023-12-11 11:42:26 +0000

    added ternary operators

---
+++ assets/js/ternaryOperators.js
@@ -0,0 +1,12 @@
+// Ternary Operators
+
+// if a customer has more than 100 points,
+// They are a 'gold' customer, otherwise,
+// They are a silver customer.
+
+
+let points = 101;
+
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
