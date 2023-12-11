14111601b81ff7630e2a4463bcfb43edd7a5ca50
Yasir Mohamed <Yasirwiifto@gmail.com>
2023-12-11 11:32:09 +0000

    added equality operators

---
+++ assets/js/equalityOperators.js
@@ -0,0 +1,12 @@
+// equality operators
+
+// strict Equality Operator (Always checks if they have the same Type and Value)
+ console.log(1 === 1);
+
+// comparing string with a number
+ console.log("1" === 1);
+
+// Lose Equality Operator (Always checks if the Values are equal)
+console.log(1 == 1);
+console.log("1" == 1);
+console.log(true == 1);
